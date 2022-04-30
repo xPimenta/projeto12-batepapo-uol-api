7aeb4e6a1c5bffb9585eaa37e99c1db8f8648a0b
Pimenta <mateuspimartins@hotmail.com>
2022-04-30 00:48:12 -0300

    template one

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from "cors"
+import { MongoClient } from "mongodb"
+import dayjs from "dayjs"
+import dotenv from "dotenv"
+
+const PORT = 5000
+
+dotenv.config()
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+let db
+mongoClient.connect(() => (db = mongoClient.db("bate_papo_uol")))
+let time
+const getTime = () => (time = dayjs().format("HH:mm:ss"))
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`)
+})
